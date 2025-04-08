50ba9eea039d26c3ebb59da26a7bebcda9d8fd4d
DreysJS <timoha.lighter130202@gmail.com>
2025-04-08 20:45:36 +0400

    prepared

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+    output: "export",
+    basePath: "/nextjs-gallery",
     experimental: {
         optimizePackageImports: ["@chakra-ui/react"],
     },
