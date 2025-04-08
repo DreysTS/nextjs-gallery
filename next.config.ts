01cb3273a72fd0d3cc68223cd329cb2921382fef
DreysJS <timoha.lighter130202@gmail.com>
2025-04-08 20:53:30 +0400

    last prepared

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const nextConfig: NextConfig = {
                 pathname: "/images/**",
             },
         ],
+        unoptimized: true,
     },
     env: {
         SERVER_URL: process.env.SERVER_URL,
